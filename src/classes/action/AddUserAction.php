f69a43e36687b0fbf0828e1747940e826b0d93fe
Gaetan Szezcpanski <gaetan.szezcpanski1@etu.univ-lorraine.fr>
2023-11-07 13:16:05 +0100

    adduseraction

--- src/classes/action/AddUserAction.php
+++ src/classes/action/AddUserAction.php
@@ -1,26 +1,36 @@
 <?php
 
 namespace iutnc\deefy\action;
-use iutnc\deefy\auth\Inscr;
 use iutnc\deefy\exception\AuthException;
+use iutnc\deefy\inscription\inscr;
+
 class AddUserAction extends Action {
 
     public function execute(): string {
         $htmlContent = '';
         if ($_SERVER['REQUEST_METHOD'] === 'GET') {
             $htmlContent = '
-            <form id="form" method="POST" action="index.php?action=register">
-                <label for="form"> email : </label>
-                <input type="email" id="email" name="email" value="axel@mail.com" required>
-                <label for="password"> mot de passe : </label>
-                <input type="text" id="password" name="password" value="Axel2004**&" required>
+            <form id="form" method="POST" action="index.php?action=register" enctype="multipart/form-data">
+                <label for="form"> Nom :</label>
+                <input type="text" id="nom" name="nom" required>
+                <label for="form"> Prenom :</label>
+                <input type="text" id="prenom" name="prenom" required>
+                <label for="form"> Email : </label>
+                <input type="email" id="email" name="email"  required>
+                <label for="password"> Mot de passe : </label>
+                <input type="text" id="password" name="password" required>
                 <input type="submit" value="Inscription">
-                ';
+                </form>
+                
+                >';
         } else if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $nom = filter_var($_POST['nom'], FILTER_SANITIZE_STRING);
+            $prenom = filter_var($_POST['prenom'], FILTER_SANITIZE_STRING);
             $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
             $password = filter_var($_POST['password']);
+
             try {
-                if (inscr::register($email, $password)) $htmlContent = 'L\'ajout du user ' . $email . ' a bien été effectué';
+                if (inscr::register($nom,$prenom,$email, $password, 0)) $htmlContent = "Vous avez créé votre compte en tant que " . $email;
             } catch (AuthException $e) {
                 echo $e;
             }
---
+++ src/classes/exception/AuthException.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+namespace iutnc\deefy\exception;
+use \Exception;
+
+class AuthException extends Exception{
+
+}
