dd6591fc865ef3ef5fbe7dcb7b0cda5cfe26859f
axeldg54 <dung1u@etu.univ-lorraine.fr>
2023-11-07 15:51:48 +0100

    modeles

---
+++ modele/user.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connexion</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background: linear-gradient(to right, #8458B3 0%, #E98181 100%);
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+        }
+        .login-container {
+            background: white;
+            padding: 2rem;
+            border-radius: 8px;
+            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+            width: 400px;
+        }
+        .login-container h2 {
+            text-align: center;
+            margin-bottom: 1rem;
+        }
+        .social-buttons {
+            display: flex;
+            justify-content: space-around;
+            margin-bottom: 1rem;
+        }
+        .social-button {
+            border: none;
+            border-radius: 4px;
+            padding: 10px 15px;
+            cursor: pointer;
+        }
+        .input-group {
+            margin-bottom: 1rem;
+        }
+        .input-group input {
+            width: 100%;
+            padding: 10px;
+            margin-top: 5px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+        }
+        .login-button {
+            width: 100%;
+            padding: 10px;
+            border: none;
+            border-radius: 4px;
+            background: black;
+            color: white;
+            cursor: pointer;
+        }
+        .footer {
+            text-align: center;
+            margin-top: 1rem;
+        }
+        .footer a {
+            text-decoration: none;
+            color: #000;
+        }
+
+        h1 {
+            position: absolute;
+            top: 10%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            color: whitesmoke;
+            font-size: 60px;
+            font-family: ;
+        }
+
+        .login-box {
+            position: relative;
+            top: -50px;
+            margin: auto;
+        }
+
+
+    </style>
+</head>
+<body>
+
+<h1>Touiteur.app</h1>
+
+<div class="login-container">
+    <h2>Bienvenue</h2>
+</div>
+
+</body>
+</html>
--- src/classes/action/SignInAction.php
+++ src/classes/action/SignInAction.php
@@ -8,19 +8,14 @@ class SignInAction extends Action {
     public function execute(): string {
         $htmlContent = '';
         if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-            $htmlContent = '
-            <form id="form" method="POST" action="index.php?action=sign-in">
-                <label for="form"> email : </label>
-                <input type="email" id="email" name="email" value="axeldung2004@gmail.com" required>
-                <label for="password"> mot de passe : </label>
-                <input type="text" id="password" name="password" value="Axeldung2004&**" required>
-                <input type="submit" value="Connexion">
-                ';
+            $htmlContent = include 'modele/connexion.php';
         } else if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
             $password = $_POST['password'];
-            if (inscr::authenticate($email, $password)) $htmlContent = 'OK, mot de passe correct et email correct';
-            else $htmlContent = 'FAUX, mot de passe ou email incorrect';
+            if (inscr::authenticate($email, $password)) {
+                $htmlContent = include 'modele/user.php';
+            }
+            else $htmlContent = include 'modele/connexionRefus.php';
         }
         return $htmlContent;
     }
