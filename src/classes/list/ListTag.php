2e460277507fa3684ebecc7a87792d35f7d22cfa
Gaetan Szezcpanski <gaetan.szezcpanski1@etu.univ-lorraine.fr>
2023-11-10 09:40:05 +0100

    reglage

--- src/classes/list/ListTag.php
+++ src/classes/list/ListTag.php
@@ -69,6 +69,9 @@ class ListTag{
                 $tag = "";
             }
         }
+        if (count($tags) === 0){
+            $tags[] = "#touiteur";
+        }
         return $tags;
     }
 }
